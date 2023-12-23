98146444cf2c5044506afb68e68af6fc1738c7e4
Jared Leander <jleander12@gmail.com>
2023-12-22 16:06:01 -0800

    Updated to add new books to the DOM with unique ID
    To do:
    - Create book entry form to populate new books
    - Add mark read functionality
    - Add a remove book functionality

--- js/script.js
+++ js/script.js
@@ -8,14 +8,32 @@ function Book(title, author, pages, read, index) {
   this.index = index;
 }
 
-function addBookToLibrary() {
-  let bookNum = myLibrary.length;    //find the end of the library to determine the next book index
-  myLibrary.push(new Book("Pandora's Star", "Peter F Hamilton", 988, true, bookNum));
-}
-
 addBookToLibrary();
 addBookToLibrary();
 addBookToLibrary();
 addBookToLibrary();
 
 console.log(myLibrary);
+
+
+
+function addBookToLibrary() {
+  let bookNum = myLibrary.length;    //find the end of the library to determine the next book index
+  myLibrary.push(new Book("Pandora's Star", "Peter F Hamilton", 988, true, bookNum));
+  newBook = myLibrary[bookNum];
+
+  const para = document.createElement("p");
+  para.setAttribute(`id`,`book ${bookNum}`);
+
+  for (const data in newBook) {
+    if(`${data}` != `index`) {
+    const list = document.createElement("li");
+    const node = document.createTextNode(`${newBook[data]}`);
+    list.appendChild(node);
+    para.appendChild(list);
+    }
+  }
+  
+  const element = document.getElementById("div1");
+  element.appendChild(para); 
+}
